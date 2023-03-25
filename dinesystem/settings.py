606bf4750cc487770d91934dca30dd9533bc3990
Ahmed Samer <ahmedsam3r@gmail.com>
2023-03-25 17:19:37 +0200

    added new app to handle tables by admin only

--- dinesystem/settings.py
+++ dinesystem/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'staff.apps.StaffConfig',
+    'table.apps.TableConfig',
 
     'rest_framework'
 ]
@@ -50,7 +51,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
